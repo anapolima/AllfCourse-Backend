42a9c4768f2088f174762bf6fce6cfbc2c9a65b4
Rey <rey.filareto@outlook.com>
2021-08-27 11:11:23 -0300

    Update loginController.js

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -24,63 +24,57 @@ module.exports = {
             },
         };
         const checkOperators = ['AND', 'AND'];
-        const result = await query.Select('users', checkSelect, whereCheck, checkOperators);
-        if (Array.isArray(result.data)) {
-            if (result.data.length >= 1) {
-                try {
-                    await Login.LoginUser(result.data[0].password, password);
-                    const token = jwt.sign(
-                        {
-                            id: result.data[0].id,
-                            email,
-                            name: result.data[0].first_name,
-                            type: result.data[0].type,
-                        },
-                        process.env.SECRET,
-                    );
-                    res.cookie('usertoken', token, {
-                        httpOnly: true,
-                    });
-                    jwt.verify(token, process.env.SECRET, (err, decoded) => {
-                        if (err) {
-                            console.log(err);
-                            errors.criticalErrors.Unauthorized = {
-                                message: 'Não autorizado',
-                                code: 401,
-                            };
-                            res.sendError(errors, 401);
-                        } else {
-                            console.log(decoded);
-                            res.status(200).send({ message: 'Login sucedido' });
-                        }
-                    });
-                } catch (err) {
-                    if (err === 1) {
-                        res.sendError({ message: 'Senha incorreta' }, 403);
-                    } else {
-                        errors.criticalErrors.errorCategory = {
-                            message: 'Ocorreu um erro inesperado.',
-                            code: 500,
-                            detail: { ...err },
+        const { rowCount: userFound } = await query.Select(
+            'users',
+            checkSelect,
+            whereCheck,
+            checkOperators,
+        );
+        if (userFound > 0) {
+            try {
+                await Login.LoginUser(userFound[0].password, password);
+                const token = jwt.sign(
+                    {
+                        id: userFound[0].id,
+                        email,
+                        name: userFound[0].first_name,
+                        type: userFound[0].type,
+                    },
+                );
+                res.cookie('usertoken', token, {
+                    httpOnly: true,
+                });
+                jwt.verify(token, process.env.SECRET, (err, decoded) => {
+                    if (err) {
+                        console.log(err);
+                        errors.criticalErrors.Unauthorized = {
+                            message: 'Não autorizado',
+                            code: 401,
                         };
-                        res.sendError(errors, 500);
+                        res.sendError(errors, 401);
+                    } else {
+                        console.log(decoded);
+                        res.status(200).send({ message: 'Login sucedido' });
                     }
+                });
+            } catch (err) {
+                if (err === 1) {
+                    res.sendError({ message: 'Senha incorreta' }, 403);
+                } else {
+                    errors.criticalErrors.errorCategory = {
+                        message: 'Ocorreu um erro inesperado.',
+                        code: 500,
+                        detail: { ...err },
+                    };
+                    res.sendError(errors, 500);
                 }
-            } else {
-                errors.criticalErrors.email = {
-                    message: 'Email não encontrado ou conta não ativada',
-                    code: 404,
-                };
-                res.sendError(errors, 404);
             }
         } else {
-            errors.criticalErrors.errorCategory = {
-                message: 'Ocorreu um erro inesperado.',
-                code: 500,
-                detail: { ...result.data },
+            errors.criticalErrors.email = {
+                message: 'Email não encontrado ou conta não ativada',
+                code: 404,
             };
-            res.sendError(errors, 500);
-            res.sendError('Erro', 500);
+            res.sendError(errors, 404);
         }
     },
 };
