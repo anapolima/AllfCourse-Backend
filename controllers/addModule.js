567a4aa44bc111a027a290d1114cd0b9af9fcb3e
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-08-27 06:05:56 -0300

    adding post modules route

---
+++ controllers/addModule.js
@@ -0,0 +1,36 @@
+const query = require('@helpers/Query');
+
+const checkAddModules = require('@functions/checkAddModule');
+
+module.exports = {
+    post: async (req, res) => {
+        const { name } = req.body;
+        const { description } = req.body;
+
+        const check = await checkAddModules.check(name, description);
+
+        if (Object.keys(check.validationErrors).length !== 0
+            || Object.keys(check.criticalErrors).length !== 0) {
+            res.sendError(check, 500);
+        } else {
+            const columns = {
+                name: check.name,
+                description: check.description,
+                created_at: 'now()',
+            };
+            const returningColumns = ['*'];
+
+            const result = await query.Insert(
+                'modules',
+                columns,
+                returningColumns,
+            );
+
+            if (Array.isArray(result.data)) {
+                res.status(201).send({ message: 'Módulo adicionado com sucesso!' });
+            } else {
+                res.sendError(result.error, 500);
+            }
+        }
+    },
+};
