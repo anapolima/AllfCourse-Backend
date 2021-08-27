e5b47392a6dca51a41a69c649c5335983d042743
Rey <rey.filareto@outlook.com>
2021-08-27 14:18:01 -0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,36 +4,44 @@ const jwt = require('@models/jwt');
 
 //  ----------------------------------------------------------------------------
 //  --------------------Middleware to user authentication-----------------------
-const authQuery = `
-      SELECT
-        users.id,
-        users.username,
-        offices.description AS office
-      FROM users
-      JOIN offices
-        ON offices.id = users.office_id
-      WHERE
-           users.id = $1
-      AND  users.username = $2
-      AND  offices.description = $3
-      AND  users.deleted_at isnull`;
-
 const authMiddleware = async (req, res, next) => {
-    // const token = req.cookies["auth"];
-    const token = req.headers['x-access-token'];
-
+    const token = req.cookies.auth;
+    // const token = req.headers['x-access-token'];
     try {
         const payload = await jwt.verify(token);
-        const { rows: userAuth } = await query(authQuery, [
-            payload.id,
-            payload.username,
-            payload.office,
-        ]);
+        const checkSelect = ['id', 'email', 'first_name', 'type'];
+        const whereCheck = {
+            email: {
+                operator: '=',
+                value: payload.email,
+            },
+            active: {
+                operator: 'is',
+                value: true,
+            },
+            deleted_at: {
+                operator: 'is',
+                value: null,
+            },
+        };
+        const checkOperators = ['AND', 'AND', 'AND'];
+
+        const { rowCount: userFound, rows: userAuth } = await query.Select(
+            'users',
+            checkSelect,
+            whereCheck,
+            checkOperators,
+        );
+
+        if (userFound === 0) {
+            return res.status(401).send('Falha na autenticação do usuário');
+        }
 
         req.auth = {
             id: userAuth[0].id,
-            username: userAuth[0].username,
-            office: userAuth[0].office,
+            email: userAuth[0].email,
+            first_name: userAuth[0].first_name,
+            type: userAuth[0].type,
         };
 
         console.table({ 'Session-user': req.auth });
