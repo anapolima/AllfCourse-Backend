1c15fc473167e51fbc11950434de117915a2e4c8
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-08-30 14:48:58 -0300

    adding get sales by student id route

--- routes/sales.routes.js
+++ routes/sales.routes.js
@@ -12,12 +12,14 @@ const router = express.Router();
 const { addSale } = require('@controller/sales/addSale.controller');
 const { getSalesByCourseId } = require('@app/controllers/sales/getSalesByCourseId.controller');
 const { getSales } = require('@app/controllers/sales/getSale.controller');
+const { getSalesByStudentId } = require('@app/controllers/sales/getSalesByStudentId.controller');
 
 // ------------------------------------------------------------//
 // -------------------------sales-routes-----------------------//
 router.post('/buy', jwtuser, addSale); // aluno, professor
 router.get('/sales/course/:id', jwtuser, getSalesByCourseId);
-router.get('/sales/:id', getSales);
+router.get('/sales/student/:id', getSalesByStudentId);
+router.get('/sales/:id', jwtuser, getSales);
 // -------------------------sales-routes-----------------------//
 // ------------------------------------------------------------//
 
