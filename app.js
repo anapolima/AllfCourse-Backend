4b7ece58ff863c06a9671fd6fb6e4e5469fde3ba
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-08-31 15:41:51 -0300

    test

--- app.js
+++ app.js
@@ -51,6 +51,14 @@ app.use(
 );
 app.use(compression());
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
 app.use(cookie_parser('1234')); // force to sign the cookie
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
