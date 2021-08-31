275157742bfd28d1202a1eeaed6ca5f81a4edcea
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-08-31 15:58:42 -0300

    improving

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(
 app.use(compression());
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Origin', `http://localhost:${process.env.front_port}`);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
