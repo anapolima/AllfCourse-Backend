7e0a8f7f97e4277803b3b71a4ebbe25fe887828b
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-08-30 23:16:21 -0300

    adding ilike and not ilike operators and removing method InsertSale

--- helpers/Query.js
+++ helpers/Query.js
@@ -226,7 +226,7 @@ class QueryGenerator {
 
                 if (whereColumns.length !== 0) {
                     whereColumns.forEach((_column, _index) => {
-                        const regex = /=$|!=$|>$|>=$|<$|<=$|between$|not between$|like$|is$|is not$|not like$|in$|not in$/i;
+                        const regex = /=$|!=$|>$|>=$|<$|<=$|between$|not between$|like$|not like$|ilike$|not ilike$|is$|is not$in$|not in$/i;
 
                         const { operator } = whereColumnsValues[_column];
                         const isValidOperator = regex.test(operator);
@@ -235,6 +235,8 @@ class QueryGenerator {
                             if (
                                 operator.toLowerCase() === 'like'
                                 || operator.toLowerCase() === 'not like'
+                                || operator.toLowerCase() === 'ilike'
+                                || operator.toLowerCase() === 'not ilike'
                             ) {
                                 whereParams.push(
                                     `${_column} ${operator.toUpperCase()} '%'||$${param}||'%' ${
@@ -577,7 +579,7 @@ class QueryGenerator {
 
                 if (whereColumns.length !== 0) {
                     whereColumns.forEach((_column, _index) => {
-                        const regex = /=$|!=$|>$|>=$|<$|<=$|between$|not between$|like$|is$|is not$|not like$/i;
+                        const regex = /=$|!=$|>$|>=$|<$|<=$|between$|not between$|like$|not like$|ilike$|not ilike$|is$|is not$in$|not in$/i;
 
                         const { operator } = whereColumnsValues[_column];
                         const isValidOperator = regex.test(operator);
@@ -586,6 +588,8 @@ class QueryGenerator {
                             if (
                                 operator.toLowerCase() === 'like'
                                 || operator.toLowerCase() === 'not like'
+                                || operator.toLowerCase() === 'ilike'
+                                || operator.toLowerCase() === 'not ilike'
                             ) {
                                 whereParams.push(
                                     `${_column} ${operator.toUpperCase()} '%'||$${param}||'%' `,
@@ -755,105 +759,6 @@ class QueryGenerator {
                         `executed query: { UPDATE ${table} SET ${this.#params} ${
                             whereColumns ? `WHERE ${this.#whereParams}` : ''
                         } ${
-                            returning ? `RETURNING ${this.#returning}` : ''
-                        }, params: ${values}; duration: ${duration}ms }\n`,
-                    );
-                    this.#columns = '';
-                    this.#params = '';
-                    this.#returning = '';
-                    this.#whereParams = '';
-                    this.#whereColumns = '';
-                    this.#orderBy = '';
-                });
-
-            return result;
-        }
-        this.#result.error.params = 'Columns and values must be arrays';
-        return this.#result;
-    }
-
-    InsertSale(_table, _columnsValues, _returning, _allow) {
-        this.#SetClient();
-        this.#result = {
-            error: {
-                transaction: false,
-                commit: false,
-                rollback: false,
-                params: false,
-            },
-            data: false,
-        };
-        const table = _table;
-        const columnsValues = _columnsValues;
-        const returning = _returning;
-        const allowcommit = _allow;
-
-        if (allowcommit === true) {
-            this.#client.connect().then(() => this.#client.query('COMMIT;'))
-                .then(() => true);
-        }
-
-        if (
-            columnsValues instanceof Object
-            && !(columnsValues instanceof Array)
-        ) {
-            const start = Date.now();
-            const columns = Object.keys(columnsValues);
-            this.#columns = columns.join(', ');
-            const params = [];
-            const values = [];
-
-            for (let i = 0; i < columns.length; i++) {
-                params.push(`$${i + 1}`);
-            }
-
-            columns.forEach((column) => {
-                values.push(columnsValues[column]);
-            });
-
-            this.#params = params.join(', ');
-
-            if (returning) {
-                if (Array.isArray(returning)) {
-                    this.#returning = returning.join(', ');
-                } else {
-                    throw new Error('Returning must be an array');
-                }
-            }
-
-            const result = this.#client
-                .connect()
-                .then(() => this.#client.query('BEGIN;'))
-                .then(() => this.#client.query(
-                    `INSERT INTO ${table} (${this.#columns}) VALUES (${
-                        this.#params
-                    }) ${returning ? `RETURNING ${this.#returning}` : ''}`,
-                    values,
-                ))
-                .then(async (result) => {
-                    this.#result.data = result.rows;
-
-                    return this.#result;
-                })
-                .catch(async (err) => {
-                    this.#result.error.transaction = err.message;
-                    await this.#client
-                        .query('ROLLBACK')
-                        .then(() => {
-                            console.log('ERROR, ROLLBACK');
-                            console.log(err);
-                        })
-                        .catch(() => (this.#result.error.rollback = true));
-                    return this.#result;
-                })
-                .finally(() => {
-                    this.#client.end();
-                    const duration = Date.now() - start;
-                    fs.appendFileSync(
-                        './logs/queries_log.log',
-                        `executed query: { INSERT INTO ${table} (${
-                            this.#columns
-                        }) VALUES (${this.#params}) ${
                             returning ? `RETURNING ${this.#returning}` : ''
                         }, params: ${values}; duration: ${duration}ms }\n`,
                     );
