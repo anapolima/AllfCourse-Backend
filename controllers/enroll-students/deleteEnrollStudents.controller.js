ebb5583bbba64b21849f069925848c5b29367237
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-08-31 00:06:15 -0300

    fixing documentation and checking permissions

--- controllers/enroll-students/deleteEnrollStudents.controller.js
+++ controllers/enroll-students/deleteEnrollStudents.controller.js
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------------------------//
 // Archive: controllers/session/login.controller.js
-// Description: File responsible for delete a class from a course
+// Description: File responsible for the 'deleteEnrollStudent' function of the 'enroll-students' class controller
 // Data: 2021/08/30
 // Author: Allfcourse team
 // -----------------------------------------------------------------------------------------------//
@@ -11,56 +11,55 @@ const checkValidCourse = require('@functions/checkIfCourseExists');
 exports.deleteEnrollStudent = async (req, res) => {
     const studentId = req.auth.id;
     const courseId = req.params.id.toString();
-    // const userType = req.auth.id;
+    const userType = req.auth.id;
 
-    // if (userType === 4 || userType === 5
-    //     || userType === 6 || userType === 7) {
-    const checkCourse = await checkValidCourse.check(courseId);
+    if (userType !== 2 && userType !== 4 && userType !== 6) {
+        const checkCourse = await checkValidCourse.check(courseId);
 
-    if (Object.keys(checkCourse.validationErrors).length !== 0
+        if (Object.keys(checkCourse.validationErrors).length !== 0
         || Object.keys(checkCourse.criticalErrors).length !== 0) {
-        res.sendError(checkCourse, 500);
-    } else {
-        const enrollColumns = {
-            deleted_at: 'now()',
-        };
-        const whereEnroll = {
-            student_id: {
-                operator: '=',
-                value: studentId,
-            },
-            course_id: {
-                operator: '=',
-                value: checkCourse.id,
-            },
-            deleted_at: {
-                operator: 'is',
-                value: 'null',
-            },
-        };
-        const logicalOperators = ['AND', 'AND'];
-        const returning = ['id', 'deleted_at'];
-
-        const enrollInformations = await query.Update(
-            true,
-            'enroll_students',
-            enrollColumns,
-            returning,
-            whereEnroll,
-            logicalOperators,
-        );
-
-        if (Array.isArray(enrollInformations.data)) {
-            if (enrollInformations.data.length > 0) {
-                res.status(200).send({ message: 'Matrícula excluída com sucesso!' });
-            } else {
-                res.sendError('Alguma coisa ocorreu errado ou não há matrícula para excluir', 500);
-            }
+            res.sendError(checkCourse, 500);
         } else {
-            res.sendError(enrollInformations.error, 500);
+            const enrollColumns = {
+                deleted_at: 'now()',
+            };
+            const whereEnroll = {
+                student_id: {
+                    operator: '=',
+                    value: studentId,
+                },
+                course_id: {
+                    operator: '=',
+                    value: checkCourse.id,
+                },
+                deleted_at: {
+                    operator: 'is',
+                    value: 'null',
+                },
+            };
+            const logicalOperators = ['AND', 'AND'];
+            const returning = ['id', 'deleted_at'];
+
+            const enrollInformations = await query.Update(
+                true,
+                'enroll_students',
+                enrollColumns,
+                returning,
+                whereEnroll,
+                logicalOperators,
+            );
+
+            if (Array.isArray(enrollInformations.data)) {
+                if (enrollInformations.data.length > 0) {
+                    res.status(200).send({ message: 'Matrícula excluída com sucesso!' });
+                } else {
+                    res.sendError('Alguma coisa ocorreu errado ou não há matrícula para excluir', 500);
+                }
+            } else {
+                res.sendError(enrollInformations.error, 500);
+            }
         }
+    } else {
+        res.status(401).send({ message: 'Não autorizado' });
     }
-    // } else {
-    //     res.status(401).send({ message: 'Não autorizado' });
-    // }
 };
