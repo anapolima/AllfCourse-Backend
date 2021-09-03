becad671299e5f2e61b1d4405b3e59fdfbdee1eb
Ana Paula Oliveira de Lima <anaolilima88@gmail.com>
2021-09-03 13:44:46 -0300

    updating the way how functions are exporteds

--- controllers/courses/addModule.controller.js
+++ controllers/courses/addModule.controller.js
@@ -2,35 +2,33 @@ const query = require('@helpers/Query');
 
 const checkAddModules = require('@functions/checkAddModule');
 
-module.exports = {
-    post: async (req, res) => {
-        const { name } = req.body;
-        const { description } = req.body;
+exports.addModule = async (req, res) => {
+    const { name } = req.body;
+    const { description } = req.body;
 
-        const check = await checkAddModules.check(name, description);
+    const check = await checkAddModules.check(name, description);
 
-        if (Object.keys(check.validationErrors).length !== 0
+    if (Object.keys(check.validationErrors).length !== 0
             || Object.keys(check.criticalErrors).length !== 0) {
-            res.sendError(check, 500);
+        res.sendError(check, 500);
+    } else {
+        const columns = {
+            name: check.name,
+            description: check.description,
+        };
+        const returningColumns = ['*'];
+
+        const result = await query.Insert(
+            true,
+            'modules',
+            columns,
+            returningColumns,
+        );
+
+        if (Array.isArray(result.data)) {
+            res.status(201).send({ message: 'Módulo adicionado com sucesso!' });
         } else {
-            const columns = {
-                name: check.name,
-                description: check.description,
-            };
-            const returningColumns = ['*'];
-
-            const result = await query.Insert(
-                true,
-                'modules',
-                columns,
-                returningColumns,
-            );
-
-            if (Array.isArray(result.data)) {
-                res.status(201).send({ message: 'Módulo adicionado com sucesso!' });
-            } else {
-                res.sendError(result.error, 500);
-            }
+            res.sendError(result.error, 500);
         }
-    },
+    }
 };
